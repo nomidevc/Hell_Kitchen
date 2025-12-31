c447dd342860a8e59dc462e77025793e3c024e0c
Duy Nam Tong <145951925+nomidevc@users.noreply.github.com>
2025-12-31 15:38:39 +0700

    feat(UI): Add LookAtCamera script and update CuttingCounter prefab
    Introduced the LookAtCamera script to allow objects to orient towards the camera in various modes.

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+   
+    [SerializeField] private Mode _mode;
+    
+    void LateUpdate()
+    {
+        switch (_mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 directionToCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + directionToCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
